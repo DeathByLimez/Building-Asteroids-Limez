8d4163484099161e88e2b6e33fafcf9ff5c0f598
DeathByLimez <thelimez@icloud.com>
2026-05-28 14:52:01 -0700

    Added all files from Logger to main

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import pygame
+
+from logger import log_state
+
+from constants import SCREEN_WIDTH
+
+from constants import SCREEN_HEIGHT
+def main():
+    print("Starting Asteroids")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+if __name__ == "__main__":
+    main()
