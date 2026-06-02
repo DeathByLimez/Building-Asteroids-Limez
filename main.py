2f6ae65501abd6a284af4f4b7275e0b47bea8529
DeathByLimez <thelimez@icloud.com>
2026-06-02 10:02:42 -0700

    Working screen

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from constants import SCREEN_WIDTH
 
 from constants import SCREEN_HEIGHT
 def main():
-    print("Starting Asteroids")
-    print("Screen width: 1280")
-    print("Screen height: 720")
-
+	pygame.init()
+	print("Starting Asteroids")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		log_state()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		pygame.display.flip()
 if __name__ == "__main__":
     main()
