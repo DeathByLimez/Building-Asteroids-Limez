bde5ee332bae800e8b1c6c76b0c435430de48f13
DeathByLimez <thelimez@icloud.com>
2026-06-03 12:33:25 -0700

    Added FPS

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from constants import SCREEN_WIDTH
 
 from constants import SCREEN_HEIGHT
 def main():
+	dt = 0.0
+	pygame.time.clock()
+	
+
+
+
 	pygame.init()
 	print("Starting Asteroids")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -15,6 +21,8 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			dt = clock.tick(60) / 1000
+			print(f"{dt}")
 		screen.fill("black")
 		pygame.display.flip()
 if __name__ == "__main__":
