6c3add4b883e94a4f7d0947fe1dee5bcf77df6f4
DeathByLimez <thelimez@icloud.com>
2026-06-03 12:56:35 -0700

    No longer prints dt

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ from constants import SCREEN_HEIGHT
 def main():
 	dt = 0.0
 	Clock = pygame.time.Clock()
-	
-
-
-
 	pygame.init()
 	print("Starting Asteroids")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,7 +18,6 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 			dt = Clock.tick(60) / 1000
-			print(f"{dt}")
 		screen.fill("black")
 		pygame.display.flip()
 if __name__ == "__main__":
