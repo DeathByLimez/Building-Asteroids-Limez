b5a24ddef0d2d3f6c8e7fbf4fdeec8f83e755086
DeathByLimez <thelimez@icloud.com>
2026-06-04 10:54:11 -0700

    Added Player Model

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-
 import pygame
 
 from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
-from constants import SCREEN_WIDTH
-
-from constants import SCREEN_HEIGHT
 def main():
 	dt = 0.0
 	Clock = pygame.time.Clock()
 	pygame.init()
 	print("Starting Asteroids")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	hero = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		log_state()
 		for event in pygame.event.get():
@@ -19,6 +18,7 @@ def main():
 				return
 			dt = Clock.tick(60) / 1000
 		screen.fill("black")
+		hero.draw(screen)
 		pygame.display.flip()
 if __name__ == "__main__":
     main()
