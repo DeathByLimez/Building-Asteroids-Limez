1d7ee1d6f2c183d8e3d5259f3dbbf2d114b2cc0e
DeathByLimez <thelimez@icloud.com>
2026-06-05 10:54:38 -0700

    Updated Groups

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from player import Player
 
 def main():
 	dt = 0.0
-	Clock = pygame.time.Clock()
+	clock = pygame.time.Clock()
 	pygame.init()
 	print("Starting Asteroids")
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	hero = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	updateable =
-	drawables =
-	Player.containers = (updateable, drawable)
+	
 	while True:
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		dt = Clock.tick(60) / 1000
-		hero.update(updateable)
+		dt = clock.tick(60) / 1000
+		updatable.update(dt)
 		screen.fill("black")
-		hero.draw(drawable)
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
 if __name__ == "__main__":
     main()
